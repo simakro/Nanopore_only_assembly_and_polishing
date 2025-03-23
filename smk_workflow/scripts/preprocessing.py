d55e27a46f8228c8e99e1b34ef9ac36714bfdbc8
simakro <74452164+simakro@users.noreply.github.com>
2025-03-23 10:15:20 +0100

    check file naming convetion in preprocessing script

--- smk_workflow/scripts/preprocessing.py
+++ smk_workflow/scripts/preprocessing.py
@@ -3,6 +3,29 @@ import sys
 import gzip
 
 
+def ensure_naming_convention(read_folder):
+    files = list(os.scandir(read_folder))
+    expected_ext = ["fastq"]
+    unexpected_files = []
+    for f in files:
+        fnm = f.path
+        ext = fnm.split(".")[-1]
+        if ext not in expected_ext:
+            unexpected_files.append(fnm)
+        else:
+            if f.path.endswith("_all.fastq"):
+                print('Input fastq files may not end on "_all.fastq"')
+                print(f.path)
+                print("violates naming convention")
+                sys.exit()
+    if not len(unexpected_files):
+        pass
+    else:
+        print(
+            f"The following unexpected files were found in input folder {unexpected_files}"
+        )
+
+
 def decompress_all(read_folder):
     files = os.scandir(read_folder)
     for f in files:
@@ -36,4 +59,5 @@ if __name__ == "__main__":
     exp_prefix = sys.argv[2]
     output = sys.argv[3]
     decompress_all(read_folder)
+    ensure_naming_convention(read_folder)
     concatenate_fastq(read_folder, exp_prefix, output)
